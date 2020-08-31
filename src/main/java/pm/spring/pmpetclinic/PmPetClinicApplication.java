6c59af703af94cb6e3af961140094bfb58917761
Piotr Maksam <pmaksam@gmail.com>
2020-08-31 17:43:21 +0200

    Initial add

---
+++ src/main/java/pm/spring/pmpetclinic/PmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package pm.spring.pmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pm/spring/pmpetclinic/PmPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pm.spring.pmpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
